9b05e483ef4a3d2f42b5990d3272472548a63831
Piyush Jain <piyushmbm45@gmail.com>
2021-09-14 07:57:58 +0530

    learning components and css link

--- reactRefresh/my-app/src/App.js
+++ reactRefresh/my-app/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
+import Goallist from './components/Goallist';
 
 function App() {
-
+  return <div className="app-div">
+    <h2>Course Goals</h2>
+    <Goallist/>
+  </div>
 }
 
 export default App;
---
+++ reactRefresh/my-app/src/components/Goallist.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Goallist.css'
+
+function Goallist() {
+  return (
+    <ul className="goal-list">
+      <li>formation</li>
+      <li>List out the api</li>
+      <li>comment on api</li>
+    </ul>
+  );
+}
+
+export default Goallist;
